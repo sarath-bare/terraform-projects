06a7c8989394ba4a8566fa0fdb5820120be4d9b1
Sarathbare <skbare12345@gmail.com>
2025-11-30 06:12:29 +0530

    hi there

--- terraform-variables/createinstance.tf
+++ terraform-variables/createinstance.tf
@@ -1,10 +1,13 @@
-resource "aws_instance" "demo_instance"{
-    ami = "ami-0fb0b230890ccd1e6"
-    instance_type = "t2.micro"
+resource "aws_instance" "demo_instance" {
+  ami           = "ami-0fb0b230890ccd1e6"
+  instance_type = "t2.micro"
 
 
-    tags = {
-        Name = "demoinstance"
-    }
+  tags = {
+    Name = "demoinstance"
+  }
+  security_groups = var.Security_Group
 
 }
+
+
--- terraform-variables/variables.tf
+++ terraform-variables/variables.tf
@@ -2,6 +2,22 @@ variable "AWS_ACCESS_KEY" {}
 
 variable "AWS_SECRET_KEY" {}
 
-varible "AWS_REGION"{
-    default = "us-east-2"
+variable "AWS_REGION" {
+  default = "us-east-1"
+
+}
+
+variable "Security_Group" {
+  type    = list(string)
+  default = ["sg-24076", "sg-90890", "sg-348838"]
+}
+
+variable "amis"{
+    type = map
+    default = {
+        us-east-1 = "ami-0b0ea68c435eb488d"
+        us-east-2 = "ami-0144d725974fe6386"
+        us-west-1 = "ami-0454207e5367abf01"
+        us-west-2 = "ami-08305dd8ab642ad8c"
+    }
 }
